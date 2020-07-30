cf03fde9677db2387a00fd6f461255fecb71f5b6
Oscar Gilaberte <oscar@runroom.com>
2020-07-30 13:54:19 +0200

    wip

--- src/Forms/Service/HubspotService.php
+++ src/Forms/Service/HubspotService.php
@@ -20,17 +20,9 @@ class HubspotService
         $array = [
             'fields' => [
                 [
-                    'name' => 'firstname',
+                    'name' => 'name',
                     'value' => 'Test',
-                ],
-                [
-                    'name' => 'lastname',
-                    'value' => 'Runroom',
-                ],
-                [
-                    'name' => 'email',
-                    'value' => 'oscar@runroom.com',
-                ],
+                ]
             ],
         ];
         $this->hubspotForms->submit('4299768', 'f754ad44-75dc-4917-9f48-b2e736f62f52', $array);
