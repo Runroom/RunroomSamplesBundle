1dd9fd17fe6fd81f8fa16e10592345b50b0b1f15
Oscar Gilaberte <oscar@runroom.com>
2020-07-30 12:57:30 +0200

    wip

--- src/Forms/Form/ContactHubspotEventHandler.php
+++ src/Forms/Form/ContactHubspotEventHandler.php
@@ -21,6 +21,13 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 
 class ContactHubspotEventHandler implements EventSubscriberInterface
 {
+    /** @var EntityManagerInterface */
+    protected $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     /**
      * @phpstan-param GenericEvent<FormAwareInterface> $event
@@ -30,8 +37,19 @@ class ContactHubspotEventHandler implements EventSubscriberInterface
     {
         $model = $event->getSubject()->getForm()->getData();
 
-        var_dump('hola');
+        $contact = new Contact();
+        $contact->setName($model->getName());
+        $contact->setEmail($model->getEmail());
+        $contact->setPhone($model->getPhone());
+        $contact->setSubject($model->getSubject());
+        $contact->setType($model->getType());
+        $contact->setPreferences($model->getPreferences());
+        $contact->setComment($model->getComment());
+        $contact->setNewsletter($model->getNewsletter());
+        $contact->setPrivacyPolicy($model->getPrivacyPolicy());
 
+        $this->entityManager->persist($contact);
+        $this->entityManager->flush();
     }
 
     public static function getSubscribedEvents(): array
