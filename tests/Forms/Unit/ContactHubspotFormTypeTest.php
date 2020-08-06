6dc947b8586278e44a65e8ddc21abaf58355b451
Oscar Gilaberte <oscar@runroom.com>
2020-08-06 12:31:42 +0200

    php-cs-fixer and type hinting

--- tests/Forms/Unit/ContactHubspotFormTypeTest.php
+++ tests/Forms/Unit/ContactHubspotFormTypeTest.php
@@ -1,28 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the RunroomSamplesBundle.
+ *
+ * (c) Runroom <runroom@runroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Runroom\SamplesBundle\Tests\Forms\Unit;
 
 use Runroom\SamplesBundle\Forms\Form\Type\ContactHubspotFormType;
 use Runroom\SamplesBundle\Forms\Model\ContactHubspot;
-use Symfony\Component\Form\Test\TypeTestCase;
 use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\Test\TypeTestCase;
 use Symfony\Component\Validator\Validation;
 
 class ContactHubspotFormTypeTest extends TypeTestCase
 {
-    protected function getExtensions()
-    {
-        $validator = Validation::createValidatorBuilder()
-            ->enableAnnotationMapping()
-            ->getValidator();
-
-        return [
-            new ValidatorExtension($validator),
-        ];
-    }
-
-    public function testSubmitValidData()
+    public function testSubmitValidData(): void
     {
         $formData = [
             'name' => null,
@@ -38,5 +37,16 @@ class ContactHubspotFormTypeTest extends TypeTestCase
         $form->submit($formData);
         $this->assertTrue($form->isSynchronized());
         $this->assertEquals($expected, $model);
+    }
+
+    protected function getExtensions(): array
+    {
+        $validator = Validation::createValidatorBuilder()
+            ->enableAnnotationMapping()
+            ->getValidator();
+
+        return [
+            new ValidatorExtension($validator),
+        ];
     }
 }
