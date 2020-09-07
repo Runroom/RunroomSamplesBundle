b88282f4cd301573ddca577a55af52326448581a
Jordi <jordism91@gmail.com>
2020-09-07 13:42:16 +0200

    Remove verbosity from phpunit

--- tests/BasicEntities/Integration/CategoryAdminTest.php
+++ tests/BasicEntities/Integration/CategoryAdminTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Runroom\SamplesBundle\Tests\Integration;
+namespace Runroom\SamplesBundle\Tests\BasicEntities\Integration;
 
 use Runroom\SamplesBundle\BasicEntities\Admin\CategoryAdmin;
 use Runroom\Testing\TestCase\SonataAdminTestCase;
