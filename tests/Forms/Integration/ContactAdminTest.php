fa675035499c0166c6c871f4fcf2df061d959e87
Jordi <jordism91@gmail.com>
2020-09-07 13:42:16 +0200

    Remove verbosity from phpunit

--- tests/Forms/Integration/ContactAdminTest.php
+++ tests/Forms/Integration/ContactAdminTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Runroom\SamplesBundle\Tests\Integration;
+namespace Runroom\SamplesBundle\Tests\Forms\Integration;
 
 use Runroom\SamplesBundle\Forms\Admin\ContactAdmin;
 use Runroom\Testing\TestCase\SonataAdminTestCase;
